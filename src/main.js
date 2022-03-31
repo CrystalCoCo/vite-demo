4d88dadd3455a217d7ff91371d8e590f935cf070
CrystalCoCo <3166146424@qq.com>
2022-03-31 15:46:23 +0800

    引入tailwind CSS

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import App from './App.vue'
 import store from './store/index'
 import router from './router/index'
 
-//import 'ant-design-vue/dist/antd.less'
+import './styles/tailwind.css';
 createApp(App).use(store).use(router).mount('#app')
 
